58b421fd3cec8c72f7bf47a40c634f8a509bca6e
Joachim Schurig <joachim>
2016-02-26 11:21:41 +0100

    Initial Commit

---
+++ rainsensor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rainsensor
+//
+//  Created by Joachim Schurig on 26.02.16.
+//  Copyright © 2016 Joachim Schurig. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

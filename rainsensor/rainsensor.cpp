32d37e5580b433039c245b2e5ca6e8106fda06fa
Joachim Schurig <joachim>
2016-04-01 11:50:48 +0200

    making size of capture area and bucket size configurable by startup options

--- rainsensor/rainsensor.cpp
+++ rainsensor/rainsensor.cpp
@@ -6,7 +6,7 @@
  
  compile:
  
- g++ -std=gnu++14 -pthread -o rainsensor rainsensor.cpp -lcppgpio
+ g++ -std=gnu++11 -pthread -o rainsensor rainsensor.cpp -lcppgpio
  
  run:
  
@@ -29,13 +29,31 @@
 #include <cppgpio.hpp>
 
 
+// keep the startup options in a struct
 
-void count_rain(const GPIO::Counter& counter, const std::string& filename, bool print_to_console, int interval = 5)
+struct option_t {
+    std::string filename;
+    bool print_to_console = false;
+    int interval = 5;
+    int gpio_pin = 0;
+    int milliliter = 5;
+    int sqcm = 127; // exact value of default device is 127.455166;
+};
+
+
+// the main loop for the rain sensor runs forever
+
+void count_rain(const option_t& options)
 {
+    // setup the Counter object with very conservative values for the debouncing
+    GPIO::Counter counter(options.gpio_pin, GPIO::GPIO_PULL::UP, std::chrono::milliseconds(500), std::chrono::milliseconds(5));
+    // and start counting
+    counter.start();
+
     typedef std::vector<unsigned long> bucket_vec_t;
     bucket_vec_t buckets;
     // assign zeroes to all buckets and make them index accessible
-    buckets.resize(60 / interval);
+    buckets.resize(60 / options.interval);
     bucket_vec_t::iterator inserter = buckets.begin();
     
     // init with current counter value (probably 0)
@@ -44,7 +62,7 @@ void count_rain(const GPIO::Counter& counter, const std::string& filename, bool 
     while (true) {
         
         // sleep some minutes
-        std::this_thread::sleep_for(std::chrono::seconds(interval*60));
+        std::this_thread::sleep_for(std::chrono::seconds(options.interval * 60));
 
         // get new counter value
         unsigned long new_event_counter = counter.get_count();
@@ -73,15 +91,15 @@ void count_rain(const GPIO::Counter& counter, const std::string& filename, bool 
             events_per_hour += *it;
         }
         
-        double mm_per_hour = events_per_hour * 127.455166 * 5 / 1000;
+        double mm_per_hour = events_per_hour * options.sqcm * options.milliliter / 1000;
         
-        if (!filename.empty()) {
+        if (!options.filename.empty()) {
             
             std::ofstream out;
-            out.open(filename.c_str(), std::ofstream::out | std::ofstream::trunc);
+            out.open(options.filename.c_str(), std::ofstream::out | std::ofstream::trunc);
 
             if (!out.is_open()) {
-                std::cerr << "Cannot open file " << filename << std::endl;
+                std::cerr << "Cannot open file " << options.filename << std::endl;
                 exit(1);
             }
 
@@ -91,7 +109,7 @@ void count_rain(const GPIO::Counter& counter, const std::string& filename, bool 
 
         }
         
-        if (print_to_console) {
+        if (options.print_to_console) {
             
             std::cout << std::setprecision(2) << std::fixed << mm_per_hour << " mm/m2" << std::endl;
             
@@ -101,55 +119,70 @@ void count_rain(const GPIO::Counter& counter, const std::string& filename, bool 
 }
 
 
+// read options and start main loop
+
 int main(int argc, char *argv[])
 {
     // analyze options
-    std::string filename;
-    bool print_to_console = false;
-    int interval = 5;
-    int gpio_pin = 0;
-    
+    option_t options;
+
     {
         int opt;
         
-        while ((opt = getopt(argc, argv, "c:f:hi:p")) != -1) {
+        while ((opt = getopt(argc, argv, "b:c:f:hi:p:s:")) != -1) {
             switch (opt) {
+                case 'b':
+                    options.milliliter = atoi(optarg);
+                    if (options.milliliter < 1 || options.milliliter > 1000) {
+                        std::cerr << "invalid value for milliliter (1..1000): " << options.milliliter << std::endl;
+                        exit(1);
+                    }
+                    break;
                 case 'c':
-                    gpio_pin = atoi(optarg);
+                    options.gpio_pin = atoi(optarg);
+                    if (options.gpio_pin < 0 || options.gpio_pin > 63) {
+                        std::cerr << "invalid value for gpio (0..63): " << options.gpio_pin << std::endl;
+                        exit(1);
+                    }
                     break;
                 case 'f':
-                    filename = optarg;
+                    options.filename = optarg;
                     break;
                 default:
                 case 'h':
                     std::cout << argv[0] << " - help:" << std::endl;
                     std::cout << std::endl;
-                    std::cout << " -c N     : select gpio pin to use (default 0)" << std::endl;
+                    std::cout << " -b N     : milliliter per bucket count (default 5)" << std::endl;
+                    std::cout << " -c N     : select gpio to use (default 0)" << std::endl;
                     std::cout << " -f file  : file to write hourly rainfall into (default none)" << std::endl;
                     std::cout << " -i N     : interval in minutes between updates (1..60, default 5)" << std::endl;
                     std::cout << " -p       : print updates to stdout too (default off)" << std::endl;
+                    std::cout << " -s N     : collector extension in square centimeters (default 127)" << std::endl;
                     std::cout << std::endl;
                     exit(0);
                 case 'i':
-                    interval = atoi(optarg);
-                    if (interval < 1 || interval > 60) {
-                        std::cerr << "invalid value for interval (1..60): " << interval << std::endl;
+                    options.interval = atoi(optarg);
+                    if (options.interval < 1 || options.interval > 60) {
+                        std::cerr << "invalid value for interval (1..60): " << options.interval << std::endl;
                         exit(1);
                     }
                     break;
                 case 'p':
-                    print_to_console = true;
+                    options.print_to_console = true;
+                    break;
+                case 's':
+                    options.sqcm = atoi(optarg);
+                    if (options.sqcm < 1 || options.sqcm > 10000) {
+                        std::cerr << "invalid value for square centimeters (1..10000): " << options.sqcm << std::endl;
+                        exit(1);
+                    }
                     break;
             }
         }
     }
 
-    // setup the Counter object
-    GPIO::Counter counter(gpio_pin, GPIO::GPIO_PULL::UP, std::chrono::milliseconds(500), std::chrono::milliseconds(5));
-    counter.start();
-    
-    // and run the endless loop to capture the rain counter
-    count_rain(counter, filename, print_to_console, interval);
+    // run the endless loop to capture the rain counter
+    count_rain(options);
     
     return 0;
 }
